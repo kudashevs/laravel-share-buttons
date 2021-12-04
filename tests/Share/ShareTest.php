9459c543ab5ed22e2fc722e9f8382742e26a8b30
Sergey Kudashev <kudashevs@gmail.com>
2021-12-04 21:03:18 +0300

    Refactor ShareTest extract share instance to test field.

--- tests/Share/ShareTest.php
+++ tests/Share/ShareTest.php
@@ -7,9 +7,18 @@ use ShareButtons\Share\Test\ExtendedTestCase;
 
 class ShareTest extends ExtendedTestCase
 {
+    private $share;
+
+    protected function setUp(): void
+    {
+        $this->share = new Share();
+
+        parent::setUp();
+    }
+
     /** @test */
     public function it_creates_self_instance_on_page()
     {
-        $this->assertInstanceOf(Share::class, (new Share())->page('https://mysite.com'));
+        $this->assertInstanceOf(Share::class, $this->share->page('https://mysite.com'));
     }
 }
