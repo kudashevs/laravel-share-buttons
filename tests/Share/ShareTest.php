e3f8b6aa0da9f97b68b4e43470b9955d1afb3e91
Sergey Kudashev <kudashevs@gmail.com>
2021-12-04 21:14:05 +0300

    Refactor CurrentPageTest move test to ShareTest and remove file.

--- tests/Share/ShareTest.php
+++ tests/Share/ShareTest.php
@@ -2,6 +2,7 @@
 
 namespace ShareButtons\Share\Test\Share;
 
+use ShareButtons\Share\Facades\ShareFacade;
 use ShareButtons\Share\Share;
 use ShareButtons\Share\Test\ExtendedTestCase;
 
@@ -26,5 +27,14 @@ class ShareTest extends ExtendedTestCase
     public function it_create_self_instance_on_current_page()
     {
         $this->assertInstanceOf(Share::class, $this->share->currentPage());
+    }
+
+    /** @test */
+    public function it_can_use_the_url_of_the_current_request()
+    {
+        $result = $this->share->currentPage()->facebook();
+        $expected = '<div id="social-links"><ul><li><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com/" class="social-button " id="" title="" rel=""><span class="fab fa-facebook-square"></span></a></li></ul></div>';
+
+        $this->assertEquals($expected, (string)$result);
     }
 }
