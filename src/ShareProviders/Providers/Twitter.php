74d9a1348315e0c6bbbe3ff97b2e6d265b1abc69
Kudashev Serge <kudashevs@gmail.com>
2021-12-06 21:52:47 +0300

    Update ShareProviders update twitter provider.

---
+++ src/ShareProviders/Providers/Twitter.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kudashevs\ShareButtons\ShareProviders\Providers;
+
+class Twitter implements ShareProvider
+{
+
+    public function buildUrl(string $url, array $options = []): string
+    {
+        $providersUrl = config('laravel-share.services.twitter.uri');
+
+        $title = empty($options['title']) ? config('laravel-share.services.twitter.text') : $options['title'];
+
+        return $providersUrl . '?text=' . urlencode($title) . '&url=' . $url;
+    }
+}
--- tests/ShareProviders/Providers/TwitterTest.php
+++ tests/ShareProviders/Providers/TwitterTest.php
@@ -5,7 +5,7 @@ namespace Kudashevs\ShareButtons\Tests\ShareProviders\Providers;
 use Kudashevs\ShareButtons\Facades\ShareButtonsFacade;
 use Kudashevs\ShareButtons\Tests\ExtendedTestCase;
 
-class TwitterShareTest extends ExtendedTestCase
+class TwitterTest extends ExtendedTestCase
 {
     /** @test */
     public function it_can_generate_a_twitter_share_link_with_default_share_text()
