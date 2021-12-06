10bf0ba5b6d474c3cd53431b45c221010f55a3b8
Kudashev Serge <kudashevs@gmail.com>
2021-12-06 21:33:04 +0300

    Update ShareProviders update linkedin provider.

---
+++ src/ShareProviders/Providers/LinkedIn.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kudashevs\ShareButtons\ShareProviders\Providers;
+
+class LinkedIn implements ShareProvider
+{
+
+    public function buildUrl(string $url, array $options = []): string
+    {
+        $title = empty($options['title']) ? config('laravel-share.services.linkedin.text') : $options['title'];
+        $summary = empty($options['summary']) ? '' : $options['summary'];
+
+        $providersUrl = config('laravel-share.services.linkedin.uri');
+        $mini = config('laravel-share.services.linkedin.extra.mini');
+
+        return $providersUrl . '?mini=' . $mini . '&url=' . $url . '&title=' . urlencode($title) . '&summary=' . urlencode($summary);
+    }
+}
