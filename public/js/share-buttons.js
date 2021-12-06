fa56ce40081cb79aba0ae2ab85180cdac63973fe
Sergey Kudashev <kudashevs@gmail.com>
2021-12-07 02:02:54 +0300

    Update share-buttons.js add document ready.

--- public/js/share-buttons.js
+++ public/js/share-buttons.js
@@ -1,19 +1,21 @@
-var popupSize = {
-    width: 780,
-    height: 550
-};
+$(document).ready(function () {
+    var popupSize = {
+        width: 780,
+        height: 550
+    };
 
-$(document).on('click', '.social-button', function (e) {
-    var verticalPos = Math.floor(($(window).width() - popupSize.width) / 2),
-        horisontalPos = Math.floor(($(window).height() - popupSize.height) / 2);
+    $(document).on('click', '.social-button', function (e) {
+        var verticalPos = Math.floor(($(window).width() - popupSize.width) / 2),
+            horisontalPos = Math.floor(($(window).height() - popupSize.height) / 2);
 
-    var popup = window.open($(this).prop('href'), 'social',
-        'width=' + popupSize.width + ',height=' + popupSize.height +
-        ',left=' + verticalPos + ',top=' + horisontalPos +
-        ',location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1');
+        var popup = window.open($(this).prop('href'), 'social',
+            'width=' + popupSize.width + ',height=' + popupSize.height +
+            ',left=' + verticalPos + ',top=' + horisontalPos +
+            ',location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1');
 
-    if (popup) {
-        popup.focus();
-        e.preventDefault();
-    }
+        if (popup) {
+            popup.focus();
+            e.preventDefault();
+        }
+    });
 });
