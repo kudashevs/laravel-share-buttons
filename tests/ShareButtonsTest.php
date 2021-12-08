a8eedc9a38b813f9d8f835ffc2827e7e7c4a698f
Sergey Kudashev <kudashevs@gmail.com>
2021-12-08 11:42:30 +0300

    Refactor ShareButtonsTest reduce the amount of tested providers in each other test.

--- tests/ShareButtonsTest.php
+++ tests/ShareButtonsTest.php
@@ -187,21 +187,17 @@ class ShareButtonsTest extends ExtendedTestCase
         $result = $this->share->page('https://mysite.com', 'My share title')
             ->facebook()
             ->twitter()
-            ->linkedin()
-            ->whatsapp()
-            ->pinterest()
-            ->reddit()
-            ->telegram();
+            ->linkedin();
 
-        $expected = '<div id="social-links"><ul><li><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com" class="social-button"><span class="fab fa-facebook-square"></span></a></li><li><a href="https://twitter.com/intent/tweet?text=My+share+title&url=https://mysite.com" class="social-button"><span class="fab fa-twitter"></span></a></li><li><a href="https://www.linkedin.com/sharing/share-offsite?mini=true&url=https://mysite.com&title=My+share+title&summary=" class="social-button"><span class="fab fa-linkedin"></span></a></li><li><a href="https://wa.me/?text=https://mysite.com" class="social-button" target="_blank"><span class="fab fa-whatsapp"></span></a></li><li><a href="https://pinterest.com/pin/create/button/?url=https://mysite.com" class="social-button"><span class="fab fa-pinterest"></span></a></li><li><a href="https://www.reddit.com/submit?title=My+share+title&url=https://mysite.com" class="social-button" target="_blank"><span class="fab fa-reddit"></span></a></li><li><a href="https://telegram.me/share/url?url=https://mysite.com&text=My+share+title" class="social-button" target="_blank"><span class="fab fa-telegram"></span></a></li></ul></div>';
+        $expected = '<div id="social-links"><ul><li><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com" class="social-button"><span class="fab fa-facebook-square"></span></a></li><li><a href="https://twitter.com/intent/tweet?text=My+share+title&url=https://mysite.com" class="social-button"><span class="fab fa-twitter"></span></a></li><li><a href="https://www.linkedin.com/sharing/share-offsite?mini=true&url=https://mysite.com&title=My+share+title&summary=" class="social-button"><span class="fab fa-linkedin"></span></a></li></ul></div>';
 
         $this->assertEquals($expected, $result);
 
         $result = $this->share->page('https://mysite.com', 'My share title')
-            ->facebook()
-            ->twitter();
+            ->reddit()
+            ->telegram();
 
-        $expected = '<div id="social-links"><ul><li><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com" class="social-button"><span class="fab fa-facebook-square"></span></a></li><li><a href="https://twitter.com/intent/tweet?text=My+share+title&url=https://mysite.com" class="social-button"><span class="fab fa-twitter"></span></a></li></ul></div>';
+        $expected = '<div id="social-links"><ul><li><a href="https://www.reddit.com/submit?title=My+share+title&url=https://mysite.com" class="social-button" target="_blank"><span class="fab fa-reddit"></span></a></li><li><a href="https://telegram.me/share/url?url=https://mysite.com&text=My+share+title" class="social-button" target="_blank"><span class="fab fa-telegram"></span></a></li></ul></div>';
 
         $this->assertEquals($expected, $result);
     }
