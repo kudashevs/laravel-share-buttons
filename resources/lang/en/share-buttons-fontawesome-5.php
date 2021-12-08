a6d4dd94956153ffcb0717727b66c44246a510a1
Sergey Kudashev <kudashevs@gmail.com>
2021-12-08 15:42:38 +0300

    Refactor share-buttons-fontawesome-5.php reorder configuration keys.

--- resources/lang/en/share-buttons-fontawesome-5.php
+++ resources/lang/en/share-buttons-fontawesome-5.php
@@ -2,10 +2,10 @@
 
 return [
     'facebook' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-facebook-square"></span></a>',
-    'twitter' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-twitter"></span></a>',
     'linkedin' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-linkedin"></span></a>',
-    'whatsapp' => '<a href=":url" class="social-button:class":id:title:rel target="_blank"><span class="fab fa-whatsapp"></span></a>',
     'pinterest' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-pinterest"></span></a>',
     'reddit' => '<a href=":url" class="social-button:class":id:title:rel target="_blank"><span class="fab fa-reddit"></span></a>',
     'telegram' => '<a href=":url" class="social-button:class":id:title:rel target="_blank"><span class="fab fa-telegram"></span></a>',
+    'twitter' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-twitter"></span></a>',
+    'whatsapp' => '<a href=":url" class="social-button:class":id:title:rel target="_blank"><span class="fab fa-whatsapp"></span></a>',
 ];
