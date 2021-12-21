99e68a3f639f5246e64dd52a108c656ce53a558e
Sergey Kudashev <kudashevs@gmail.com>
2021-12-22 01:32:36 +0300

    Refactor config move link templates to the configuration file.

--- config/share-buttons.php
+++ config/share-buttons.php
@@ -77,6 +77,33 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Templates
+    |--------------------------------------------------------------------------
+    |
+    | This value specifies link templates for share providers.
+    | The format of substituted elements depends on the current package templater.
+    |
+    |
+    */
+
+    'templates' => [
+        'copylink' => '<a href=":url" class="social-button:class" id="clip":title:rel><span class="fa fa-share"></span></a>',
+        'evernote' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-evernote"></span></a>',
+        'facebook' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-facebook-square"></span></a>',
+        'hackernews' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-hacker-news"></span></a>',
+        'linkedin' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-linkedin"></span></a>',
+        'pinterest' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-pinterest"></span></a>',
+        'pocket' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-get-pocket"></span></a>',
+        'reddit' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-reddit"></span></a>',
+        'skype' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-skype"></span></a>',
+        'telegram' => '<a href=":url" class="social-button:class":id:title:rel target="_blank"><span class="fab fa-telegram"></span></a>',
+        'twitter' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-twitter"></span></a>',
+        'vkontakte' => '<a href=":url" class="social-button:class":id:title:rel><span class="fab fa-vk"></span></a>',
+        'whatsapp' => '<a href=":url" class="social-button:class":id:title:rel target="_blank"><span class="fab fa-whatsapp"></span></a>',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
     | Font Awesome version
     |--------------------------------------------------------------------------
     |
