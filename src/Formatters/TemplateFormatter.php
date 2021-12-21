73bf789457762683749402ba740e30100f1bad71
Sergey Kudashev <kudashevs@gmail.com>
2021-12-22 01:04:12 +0300

    Update Formatter add TemplateFormatter class.

---
+++ src/Formatters/TemplateFormatter.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace Kudashevs\ShareButtons\Formatters;
+
+use Kudashevs\ShareButtons\Templaters\ColonTemplater;
+use Kudashevs\ShareButtons\Templaters\Templater;
+
+class TemplateFormatter implements Formatter
+{
+    /**
+     * @var Templater
+     */
+    private $templater;
+
+    /**
+     * Contain formatter options.
+     *
+     * @var array
+     */
+    private $options = [
+        'block_prefix' => null,
+        'block_suffix' => null,
+        'element_prefix' => null,
+        'element_suffix' => null,
+    ];
+
+    /**
+     * TemplateFormatter constructor.
+     *
+     * @param array $options
+     */
+    public function __construct(array $options = [])
+    {
+        $this->templater = $this->createTemplater();
+
+        $this->updateOptions($options);
+    }
+
+    /**
+     * @return Templater
+     */
+    private function createTemplater(): Templater
+    {
+        return new ColonTemplater();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function updateOptions(array $options): void
+    {
+        $this->initFormatterStyling($options);
+        $this->initElementStyling($options);
+    }
+
+    /**
+     * @param array $options
+     */
+    private function initFormatterStyling(array $options): void
+    {
+        $this->options['block_prefix'] = $this->setFormatterBlockPrefix($options);
+        $this->options['block_suffix'] = $this->setFormatterBlockSuffix($options);
+        $this->options['element_prefix'] = $this->setFormatterElementPrefix($options);
+        $this->options['element_suffix'] = $this->setFormatterElementSuffix($options);
+    }
+
+    /**
+     * @param array $options
+     * @return string
+     */
+    private function setFormatterBlockPrefix(array $options): string
+    {
+        return $options['block_prefix'] ?? config('share-buttons.block_prefix', '<ul>');
+    }
+
+    /**
+     * @param array $options
+     * @return string
+     */
+    private function setFormatterBlockSuffix(array $options): string
+    {
+        return $options['block_suffix'] ?? config('share-buttons.block_suffix', '</ul>');
+    }
+
+    /**
+     * @param array $options
+     * @return string
+     */
+    private function setFormatterElementPrefix(array $options): string
+    {
+        return $options['element_prefix'] ?? config('share-buttons.element_prefix', '<li>');
+    }
+
+    /**
+     * @param array $options
+     * @return string
+     */
+    private function setFormatterElementSuffix(array $options): string
+    {
+        return $options['element_suffix'] ?? config('share-buttons.element_suffix', '</li>');
+    }
+
+    /**
+     * @param array $options
+     */
+    private function initElementStyling(array $options): void
+    {
+        $allowed = $this->filterElementStyling($options);
+
+        $this->options = array_merge($this->options, $allowed);
+    }
+
+    /**
+     * @param array $options
+     * @return array
+     */
+    private function filterElementStyling(array $options): array
+    {
+        $elementStyling = [
+            'class' => '',
+            'id' => '',
+            'title' => '',
+            'rel' => '',
+        ];
+
+        return array_intersect_key($options, $elementStyling);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function formatElement(string $provider, string $url, array $options = []): string
+    {
+        $providerArguments = $this->filterElementStyling($options);
+
+        return $this->generateLink($provider, $url, $providerArguments);
+    }
+
+    /**
+     * @param string $provider
+     * @param string $url
+     * @param array $options
+     * @return string
+     */
+    private function generateLink(string $provider, string $url, array $options): string
+    {
+        $template = $this->prepareElementTemplate($provider);
+        $styling = $this->prepareElementStyling($url, $options);
+
+        return $this->templater->process($template, $styling);
+    }
+
+    /**
+     * @param string $provider
+     * @return string
+     */
+    private function prepareElementTemplate(string $provider): string
+    {
+        return config('share-buttons.templates.' . $provider);
+    }
+
+    /**
+     * @param string $url
+     * @param array $options
+     * @return array
+     */
+    private function prepareElementStyling(string $url, array $options): array
+    {
+        $styling = array_merge($this->options, $options);
+
+        return [
+            'url' => $url,
+            'class' => !empty($styling['class']) ? (' ' . $styling['class']) : '',
+            'id' => !empty($styling['id']) ? (' id="' . $styling['id'] . '"') : '',
+            'title' => !empty($styling['title']) ? (' title="' . $styling['title'] . '"') : '',
+            'rel' => !empty($styling['rel']) ? (' rel="' . $styling['rel'] . '"') : '',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getBlockPrefix(): string
+    {
+        return $this->options['block_prefix'];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getBlockSuffix(): string
+    {
+        return $this->options['block_suffix'];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getElementPrefix(): string
+    {
+        return $this->options['element_prefix'];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getElementSuffix(): string
+    {
+        return $this->options['element_suffix'];
+    }
+}
---
+++ tests/Formatters/TemplateFormatterTest.php
@@ -0,0 +1,324 @@
+<?php
+
+namespace Kudashevs\ShareButtons\Tests\Formatters;
+
+use Kudashevs\ShareButtons\Formatters\TemplateFormatter;
+use Kudashevs\ShareButtons\Tests\ExtendedTestCase;
+
+class TemplateFormatterTest extends ExtendedTestCase
+{
+    /**
+     * @var TemplateFormatter
+     */
+    private $formatter;
+
+    protected function setUp(): void
+    {
+        parent::setUp(); // it goes first to initialize a container
+
+        $this->formatter = new TemplateFormatter();
+    }
+
+    /** @test */
+    public function it_can_return_options()
+    {
+        $result = $this->formatter->getOptions();
+
+        $this->assertNotEmpty($result);
+    }
+
+    /**
+     * @test
+     * @dataProvider provide_different_formatter_setup_from_options
+     * @param array $options
+     * @param string $key
+     * @param string $expected
+     */
+    public function it_can_setup_value_from_options(array $options, string $key, string $expected)
+    {
+        $this->formatter->updateOptions($options);
+
+        $result = $this->formatter->getOptions();
+
+        $this->assertArrayHasKey($key, $result);
+        $this->assertSame($expected, $result[$key]);
+    }
+
+    public function provide_different_formatter_setup_from_options()
+    {
+        return [
+            'block_prefix option is not empty' => [
+                ['block_prefix' => '<div>'],
+                'block_prefix',
+                '<div>',
+            ],
+            'block_prefix option is empty' => [
+                ['block_prefix' => ''],
+                'block_prefix',
+                '',
+            ],
+            'block_suffix option is not empty' => [
+                ['block_suffix' => '</div>'],
+                'block_suffix',
+                '</div>',
+            ],
+            'block_suffix option is empty' => [
+                ['block_suffix' => ''],
+                'block_suffix',
+                '',
+            ],
+            'element_prefix option is not empty' => [
+                ['element_prefix' => '<p>'],
+                'element_prefix',
+                '<p>',
+            ],
+            'element_prefix option is empty' => [
+                ['element_prefix' => ''],
+                'element_prefix',
+                '',
+            ],
+            'element_suffix option is not empty' => [
+                ['element_suffix' => '</p>'],
+                'element_suffix',
+                '</p>',
+            ],
+            'element_suffix option is empty' => [
+                ['element_suffix' => ''],
+                'element_suffix',
+                '',
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider provide_different_formatter_setup_from_config
+     * @param string $configuration
+     * @param string $key
+     */
+    public function it_can_setup_default_value_without_options(string $configuration, string $key)
+    {
+        $expected = config($configuration);
+
+        $result = $this->formatter->getOptions();
+
+        $this->assertArrayHasKey($key, $result);
+        $this->assertSame($expected, $result[$key]);
+    }
+
+    public function provide_different_formatter_setup_from_config()
+    {
+        return [
+            'default block_prefix' => [
+                'share-buttons.block_prefix',
+                'block_prefix',
+            ],
+            'default block_suffix' => [
+                'share-buttons.block_suffix',
+                'block_suffix',
+            ],
+            'default element_prefix' => [
+                'share-buttons.element_prefix',
+                'element_prefix',
+            ],
+            'default element_suffix' => [
+                'share-buttons.element_suffix',
+                'element_suffix',
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider provide_different_formatter_setup_from_options_for_styling
+     * @param array $options
+     * @param string $method
+     * @param string $expected
+     */
+    public function it_can_return_formatter_styling_from_options(array $options, string $method, string $expected)
+    {
+        $this->formatter->updateOptions($options);
+
+        $result = $this->formatter->$method();
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function provide_different_formatter_setup_from_options_for_styling()
+    {
+        return [
+            'getBlockPrefix method' => [
+                ['block_prefix' => '<p>'],
+                'getBlockPrefix',
+                '<p>',
+            ],
+            'getBlockSuffix method' => [
+                ['block_suffix' => '</p>'],
+                'getBlockSuffix',
+                '</p>',
+            ],
+            'getElementPrefix method' => [
+                ['element_prefix' => '<article>'],
+                'getElementPrefix',
+                '<article>',
+            ],
+            'getElementSuffix method' => [
+                ['element_suffix' => '</article>'],
+                'getElementSuffix',
+                '</article>',
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider provide_different_formatter_setup_from_config_for_styling
+     * @param string $configuration
+     * @param string $method
+     */
+   public function it_can_return_formatter_styling_without_options(string $configuration, string $method)
+    {
+        $expected = config($configuration);
+
+        $result = $this->formatter->$method();
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function provide_different_formatter_setup_from_config_for_styling()
+    {
+        return [
+            'getBlockPrefix method' => [
+                'share-buttons.block_prefix',
+                'getBlockPrefix',
+            ],
+            'getBlockSuffix method' => [
+                'share-buttons.block_suffix',
+                'getBlockSuffix',
+            ],
+            'getElementPrefix method' => [
+                'share-buttons.element_prefix',
+                'getElementPrefix',
+            ],
+            'getElementSuffix method' => [
+                'share-buttons.element_suffix',
+                'getElementSuffix',
+            ],
+        ];
+    }
+
+    /** @test */
+    public function it_can_format_an_element_with_default_styling()
+    {
+        $expected = '<li><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com" class="social-button"><span class="fab fa-facebook-square"></span></a></li>';
+
+        $result = $this->formatter->formatElement('facebook',
+            'https://www.facebook.com/sharer/sharer.php?u=https://mysite.com', []);
+
+        $this->assertNotEmpty($result);
+        $this->assertEquals($expected, $this->applyElementWrapping($result));
+    }
+
+    /** @test */
+    public function it_can_format_an_element_with_custom_styling_from_formatter_options()
+    {
+        $this->formatter->updateOptions(['element_prefix' => '<p>', 'element_suffix' => '</p>']);
+
+        $expected = '<p><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com" class="social-button"><span class="fab fa-facebook-square"></span></a></p>';
+
+        $result = $this->formatter->formatElement('facebook',
+            'https://www.facebook.com/sharer/sharer.php?u=https://mysite.com', []);
+
+        $this->assertNotEmpty($result);
+        $this->assertEquals($expected, $this->applyElementWrapping($result));
+    }
+
+    /**
+     * @test
+     * @dataProvider provide_different_styling_for_a_link
+     */
+    public function it_can_format_a_link_with_custom_styling_from_call_options($url, $options, $expected)
+    {
+        $result = $this->formatter->formatElement('facebook', $url, $options);
+
+        $this->assertEquals($expected, $this->applyElementWrapping($result));
+    }
+
+    public function provide_different_styling_for_a_link()
+    {
+        return [
+            'check class option' => [
+                'https://www.facebook.com/sharer/sharer.php?u=https://mysite.com',
+                ['class' => 'tested'],
+                '<li><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com" class="social-button tested"><span class="fab fa-facebook-square"></span></a></li>',
+            ],
+            'check id option' => [
+                'https://www.facebook.com/sharer/sharer.php?u=https://mysite.com',
+                ['id' => 'tested'],
+                '<li><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com" class="social-button" id="tested"><span class="fab fa-facebook-square"></span></a></li>',
+            ],
+            'check title option' => [
+                'https://www.facebook.com/sharer/sharer.php?u=https://mysite.com',
+                ['title' => 'tested'],
+                '<li><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com" class="social-button" title="tested"><span class="fab fa-facebook-square"></span></a></li>',
+            ],
+            'check rel option' => [
+                'https://www.facebook.com/sharer/sharer.php?u=https://mysite.com',
+                ['rel' => 'nofollow'],
+                '<li><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com" class="social-button" rel="nofollow"><span class="fab fa-facebook-square"></span></a></li>',
+            ],
+            'check mass options' => [
+                'https://www.facebook.com/sharer/sharer.php?u=https://mysite.com',
+                ['rel' => 'nofollow', 'title' => 'Title', 'id' => 'click', 'class' => 'hover active'],
+                '<li><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com" class="social-button hover active" id="click" title="Title" rel="nofollow"><span class="fab fa-facebook-square"></span></a></li>',
+            ],
+        ];
+    }
+
+    /** @test */
+    public function it_cannot_format_an_element_with_custom_styling_from_call_options()
+    {
+        $expected = '<li><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com" class="social-button"><span class="fab fa-facebook-square"></span></a></li>';
+
+        $result = $this->formatter->formatElement('facebook',
+            'https://www.facebook.com/sharer/sharer.php?u=https://mysite.com',
+            ['element_prefix' => '<p>', 'element_suffix' => '</p>']);
+
+        $this->assertNotEmpty($result);
+        $this->assertEquals($expected, $this->applyElementWrapping($result));
+    }
+
+    /** @test */
+    public function it_cannot_override_arguments_by_options()
+    {
+        $expected = '<li><a href="https://www.facebook.com/sharer/sharer.php?u=https://mysite.com" class="social-button arguments" id="arguments" title="arguments" rel="arguments"><span class="fab fa-facebook-square"></span></a></li>';
+        $this->formatter->updateOptions([
+            'class' => 'options',
+            'id' => 'options',
+            'title' => 'options',
+            'rel' => 'options',
+        ]);
+
+        $result = $this->formatter->formatElement(
+            'facebook',
+            'https://www.facebook.com/sharer/sharer.php?u=https://mysite.com',
+            [
+                'class' => 'arguments',
+                'id' => 'arguments',
+                'title' => 'arguments',
+                'rel' => 'arguments',
+            ]);
+
+        $this->assertNotEmpty($result);
+        $this->assertEquals($expected, $this->applyElementWrapping($result));
+    }
+
+    /**
+     * @param string $result
+     * @return string
+     */
+    private function applyElementWrapping(string $result): string
+    {
+        return $this->formatter->getElementPrefix() . $result . $this->formatter->getElementSuffix();
+    }
+}
