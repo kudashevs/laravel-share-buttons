989b2491a8ce762039a5e4a0f217f23b09303714
Sergey Kudashev <kudashevs@gmail.com>
2022-11-04 21:43:10 +0300

    Refactor ShareProviderTest remove a redundant check in the cannot instantiate with a wrong name test.

--- tests/ShareProviders/ShareProviderTest.php
+++ tests/ShareProviders/ShareProviderTest.php
@@ -12,11 +12,6 @@ class ShareProviderTest extends TestCase
    /** @test */
     public function it_cannot_instantiate_a_provider_with_an_unknown_name()
     {
-        /**
-         * We check it here just to be sure that the testing class is a right descendant.
-         */
-        $this->assertTrue(is_subclass_of(CopyLink::class, ShareProvider::class));
-
         $this->expectException(InvalidProviderException::class);
         $this->expectExceptionMessage('wrong is not');
 
