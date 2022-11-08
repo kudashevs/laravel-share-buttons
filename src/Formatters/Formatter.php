60e7456abf6442fee58665c5d0ebc45ed9a36bf9
Sergey Kudashev <kudashevs@gmail.com>
2022-11-08 20:41:52 +0300

    Refactor Formatter reorder abstraction methods.

--- src/Formatters/Formatter.php
+++ src/Formatters/Formatter.php
@@ -5,18 +5,23 @@ namespace Kudashevs\ShareButtons\Formatters;
 interface Formatter
 {
     /**
-     * @param array $options
-     * @return void|bool
-     */
-    public function updateOptions(array $options);
-
-    /**
      * @param string $provider
      * @param string $url
      * @param array $options
      * @return string
      */
     public function formatElement(string $provider, string $url, array $options): string;
+
+    /**
+     * @return array
+     */
+    public function getOptions(): array;
+
+    /**
+     * @param array $options
+     * @return void|bool
+     */
+    public function updateOptions(array $options);
 
     /**
      * @return string
