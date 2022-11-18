d7ae2b4b8f4bbe677526464c24a75c54365eb959
Sergey Kudashev <kudashevs@gmail.com>
2022-11-18 21:16:07 +0300

    Refactor TemplatingShareButtonsPresenter reorder class methods.

--- src/Presenters/TemplatingShareButtonsPresenter.php
+++ src/Presenters/TemplatingShareButtonsPresenter.php
@@ -65,6 +65,14 @@ class TemplatingShareButtonsPresenter implements ShareButtonsPresenter
         $this->initElementAttributes($applicable);
     }
 
+    /**
+     * @return array<string, string>
+     */
+    protected function retrieveApplicableOptions(array $options): array
+    {
+        return array_filter($options, 'is_string');
+    }
+
     private function initBlockWrappers(array $options): void
     {
         $this->styling['block_prefix'] = $options['block_prefix'] ?? config('share-buttons.block_prefix', '<ul>');
@@ -80,14 +88,6 @@ class TemplatingShareButtonsPresenter implements ShareButtonsPresenter
     private function initElementAttributes(array $options): void
     {
         $this->attributes = array_diff_key($options, $this->styling);
-    }
-
-    /**
-     * @return array<string, string>
-     */
-    protected function retrieveApplicableOptions(array $options): array
-    {
-        return array_filter($options, 'is_string');
     }
 
     /**
