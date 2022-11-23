ca60fd4d7e71727fcf998778229fede28dfdd63f
Sergey Kudashev <kudashevs@gmail.com>
2022-11-23 21:27:54 +0300

    Update UrlProviders add a new UrlProvider abstraction.

---
+++ src/UrlProviders/UrlProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kudashevs\ShareButtons\UrlProviders;
+
+/**
+ * ShareProvider represents an abstraction that generates a ready-to-use share button URL.
+ */
+interface UrlProvider
+{
+    /**
+     * Return a share provider's ready-to-use URL.
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return string
+     */
+    public function generateUrl(string $name, array $arguments): string;
+}
