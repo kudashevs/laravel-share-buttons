89704932b9b64d7d5054c020bce9a874c9c59768
Sergey Kudashev <kudashevs@gmail.com>
2022-12-03 11:32:54 +0300

    Style config file update the react on calls section comment.

--- config/share-buttons.php
+++ config/share-buttons.php
@@ -130,13 +130,16 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | React on errors
+    | React on unexpected calls
     |--------------------------------------------------------------------------
     |
-    | This package uses a magic __call() method in its core. Despite the fact
-    | that this solution works pretty fine, it can lead to hard to find errors.
-    | If you want to be aware of all the unexpected calls on the __call() method,
-    | set the below option "reactOnErrors" to `true`. If not, set it to `false`.
+    | This package uses a magic __call() method (the method which is triggered
+    | every time when a non-existing method or inaccessible method is invoked).
+    | Therefore, the package will process all of the invoked methods, even if
+    | the method name is wrong. If you want to be aware of all unexpected calls,
+    | you can use different options to report them:
+    | - 'reportUnexpectedCalls' set to true will raise the BadMethodCallException
+    |   exception on every unexpected call.
     |
     */
 
