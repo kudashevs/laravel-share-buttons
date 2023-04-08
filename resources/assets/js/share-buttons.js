0f243f0b22ed2d3e21fc9d01dfe93a25cf1b455c
Sergey Kudashev <kudashevs@gmail.com>
2023-04-08 17:51:53 +0300

    Update share-buttons.js add a vanilla JS implementation.

--- resources/assets/js/share-buttons.js
+++ resources/assets/js/share-buttons.js
@@ -1,8 +1,14 @@
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", () => {
     const popupWidth = 780;
     const popupHeight = 550;
 
-    $(document).on('click', '.social-button', function (e) {
+    let socialButtons = document.querySelectorAll('.social-button');
+
+    socialButtons.forEach((social) => {
+        social.addEventListener('click', clickHandler)
+    })
+
+    function clickHandler(e) {
         if ((e.target.id || e.target.parentElement.id) === 'clip') {
             e.preventDefault();
             if (window.clipboardData && window.clipboardData.setData) {
@@ -18,10 +24,13 @@ $(document).ready(function () {
             return;
         }
 
-        let vPosition = Math.floor(($(window).width() - popupWidth) / 2),
-            hPosition = Math.floor(($(window).height() - popupHeight) / 2);
+        let windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        let windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-        let popup = window.open($(this).prop('href'), 'social',
+        let vPosition = Math.floor((windowWidth - popupWidth) / 2),
+            hPosition = Math.floor((windowHeight - popupHeight) / 2);
+
+        let popup = window.open(this.href, 'social',
             'width=' + popupWidth + ',height=' + popupHeight +
             ',left=' + vPosition + ',top=' + hPosition +
             ',location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1');
@@ -30,5 +39,5 @@ $(document).ready(function () {
             popup.focus();
             e.preventDefault();
         }
-    });
+    }
 });
