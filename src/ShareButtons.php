aba28c3a5f2091ee2952017deb3d612b5dd05e0c
Sergey Kudashev <kudashevs@gmail.com>
2023-06-24 09:27:11 +0300

    Style ShareButtons change the comment format in the rememberProcessedCall method

--- src/ShareButtons.php
+++ src/ShareButtons.php
@@ -212,10 +212,8 @@ class ShareButtons
 
     protected function rememberProcessedCall(string $name, array $arguments = []): void
     {
-        /**
-         * Since a share button can be displayed only once, there is no need to keep track and
-         * make sure that the information about a previous button's call might be overwritten.
-         */
+        // Since a share button can be displayed only once, there is no need to keep track and
+        // make sure that the information about a previous button's call might be overwritten.
         $this->calls[$name] = new ProcessedCall($name, $arguments);
     }
 
