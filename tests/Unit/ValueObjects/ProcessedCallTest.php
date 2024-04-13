823fb737cb0ac9200b50898dce376570d794789a
Sergey Kudashev <kudashevs@gmail.com>
2024-04-13 14:09:51 +0300

    Update ProcessedCallTest add a provideRequiredArguments helper method

--- tests/Unit/ValueObjects/ProcessedCallTest.php
+++ tests/Unit/ValueObjects/ProcessedCallTest.php
@@ -19,29 +19,41 @@ class ProcessedCallTest extends ExtendedTestCase
         $this->expectException(InvalidProcessedCallArgument::class);
         $this->expectExceptionMessage('empty');
 
-        new ProcessedCall('', []);
+        new ProcessedCall('', $this->generateRequiredArguments());
     }
 
     /** @test */
     public function it_creates_an_object_with_a_name_only(): void
     {
         $name = 'twitter';
+        $arguments = $this->generateRequiredArguments();
 
-        $instance = new ProcessedCall($name, []);
+        $instance = new ProcessedCall($name, $arguments);
 
         $this->assertSame($name, $instance->getName());
-        $this->assertEmpty($instance->getArguments());
+        $this->assertSame($arguments, $instance->getArguments());
     }
 
     /** @test */
     public function it_creates_an_object_with_the_provided_state(): void
     {
         $name = 'facebook';
-        $options = ['title' => 'test'];
+        $arguments = array_merge(['title' => 'test'], $this->generateRequiredArguments());
 
-        $instance = new ProcessedCall($name, $options);
+        $instance = new ProcessedCall($name, $arguments);
 
         $this->assertSame($name, $instance->getName());
-        $this->assertSame($options, $instance->getArguments());
+        $this->assertSame($arguments, $instance->getArguments());
+    }
+
+    /**
+     * @return array{url: string, text: string}
+     */
+    private function generateRequiredArguments(): array
+    {
+        return [
+            'url' => 'any',
+            'text' => 'any',
+        ];
     }
 }
